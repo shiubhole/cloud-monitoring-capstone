73bd2ed5a69e922ac4f9b7c9fe89835b2f03ec24
shivani <shivanibhole7@gmail.com>
2026-04-15 18:06:56 +0530

    update IAM role

--- modules/jenkins/main.tf
+++ modules/jenkins/main.tf
@@ -24,6 +24,11 @@ resource "aws_iam_role_policy_attachment" "ssm_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+resource "aws_iam_role_policy_attachment" "admin_policy" {
+  role       = aws_iam_role.jenkins_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
 resource "aws_iam_instance_profile" "jenkins_profile" {
   name = "jenkins-ssm-profile"
   role = aws_iam_role.jenkins_ssm_role.name
